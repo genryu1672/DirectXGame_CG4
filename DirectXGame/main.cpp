f9dbb75786a07658d03a418a347cbf3640383a82
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-04-23 11:39:52 +0900

    CG5_00_01 P39までやった。

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,6 +1,8 @@
 #include <Windows.h>
 #include"KamataEngine.h"
 #include"GameScene.h"
+#include<cassert>
+#include<d3dcompiler.h>
 //グローバル関数
 using namespace KamataEngine;
 
@@ -25,6 +27,95 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	GameScene* gameScene = new GameScene();
 	//ゲームシーンの初期化
 	gameScene->Initialize();
+
+	//RootSignature作成
+	//構造体にデータを用意する
+	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
+	descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+	ID3DBlob* signatureBlob = nullptr;
+	ID3DBlob* errorBlog = nullptr;
+	HRESULT hr = D3D12SerializeRootSignature(&descriptionRootSignature, D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlog);
+
+	if (FAILED(hr))
+	{
+		DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<char*>(errorBlog->GetBufferPointer()));
+		assert(false);
+	}
+
+	//バイナリをもとに生成
+	ID3D12RootSignature* rootSignature = nullptr;
+	hr = dxCommon->GetDevice()->CreateRootSignature(0, signatureBlob->GetBufferPointer(), signatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
+	assert(SUCCEEDED(hr));
+
+	//InputLayout
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
+	inputElementDescs[0].SemanticName = "POSITION";
+	inputElementDescs[0].SemanticIndex = 0;
+	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
+	inputLayoutDesc.pInputElementDescs = inputElementDescs;
+	inputLayoutDesc.NumElements = _countof(inputElementDescs);
+
+	//BlendState(今回は不透明)
+	D3D12_BLEND_DESC blendDesc{};
+	//全ての色要素を書き込む
+	blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+
+	//RasterizerState
+	D3D12_RASTERIZER_DESC rasterizerDesc{};
+	//裏面(反時計回り)をカリングする
+	rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
+	//塗りつぶしモードをソリッドにする(ワイヤーフレームならD3D12_FILE_MODE_WIREFRAME)
+	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
+
+	//コンパイル済みのShader、エラー時情報の格納場所の用意
+	ID3DBlob* vsBlob = nullptr;//頂点シェーダーオブジェクト
+	ID3DBlob* psBlob = nullptr;//ピクセルシェーダオブジェクト
+	ID3DBlob* errorBlob = nullptr;//エラーオブジェクト
+
+	//頂点シェーダの読み込みとコンパイル
+	std::wstring vsFile = L"Resource/shaders/TestVS.hlsl";
+	hr = D3DCompileFromFile(
+	    vsFile.c_str(), // シェーダファイル名
+	    nullptr,
+	    D3D_COMPILE_STANDARD_FILE_INCLUDE,               // インクルード可能にする
+	    "main", "vs_5_0",                                // エントリーポイント名、シェーダモデル指定
+	    D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバック用設定
+	    0, &vsBlob, &errorBlob);
+	if (FAILED(hr))
+	{
+		DebugText::GetInstance()->ConsolePrintf(std::system_category().message(hr).c_str());
+		if (errorBlob)
+		{
+			DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
+		}
+		assert(false);
+	}
+
+	//PSO(PipelineStateObject)の生成
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPiplineStateDesc{};
+	graphicsPiplineStateDesc.pRootSignature = rootSignature;//RootSignature
+	graphicsPiplineStateDesc.InputLayout = inputLayoutDesc;//InputLayout
+	graphicsPiplineStateDesc.VS = {vsBlob->GetBufferPointer(), vsBlob->GetBufferSize()};//VertexShader
+	graphicsPiplineStateDesc.PS = {psBlob->GetBufferPointer(), psBlob->GetBufferSize()};//PixelShader
+	graphicsPiplineStateDesc.BlendState = blendDesc;//BlendState
+	graphicsPiplineStateDesc.RasterizerState = rasterizerDesc;//RasterizerState
+
+	//書き込むRTVの情報
+	graphicsPiplineStateDesc.NumRenderTargets = 1;//1つのRTVに書き込む　※２つ同時にしようと思えば出来る
+	graphicsPiplineStateDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	//利用するトポロジ(形状)のタイプ。三角形
+	graphicsPiplineStateDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	//どのように画面に色を打ち込むかの設定（今気にしなくていい）
+	graphicsPiplineStateDesc.SampleDesc.Count = 1;
+	graphicsPiplineStateDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
+	//準備は整った。PSOを生成する
+	ID3D12PipelineState* graphicsPiplineState = nullptr;
+	hr = dxCommon->GetDevice()->CreateGraphicsPipelineState(
+		&graphicsPiplineStateDesc,IID_PPV_ARGS(&graphicsPiplineState));
+	assert(SUCCEEDED(hr));
+
 
 	//メインループ
 	while (true) {
