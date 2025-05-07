8c501fd171b1af4c9aa6d3ffe9b3f3e5eae2ac32
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-05-07 11:34:36 +0900

    CG4_02_04 P6までやった。

--- DirectXGame/Particle.cpp
+++ DirectXGame/Particle.cpp
@@ -4,7 +4,7 @@
 
 using namespace MathUtility;
 
-void Particle::Initialize(Model* model, Vector3 position) {
+void Particle::Initialize(Model* model, Vector3 position, Vector3 velocity) {
 	//NULLポインタチェック①
 	assert(model);
 	//引数として受け取ったデータをメンバ変数に記録する②
@@ -16,6 +16,9 @@ void Particle::Initialize(Model* model, Vector3 position) {
 	color_ = {1, 1, 0, 1};
 
 	worldTransform_.translation_ = position;
+
+	//受け取った移動量をメンバ変数に代入する
+	velocity_ = velocity;
 }
 
 void Particle::Update() 
@@ -23,7 +26,7 @@ void Particle::Update()
 	//色変更オブジェクトに色の数値を設定する
 	objectColor_.SetColor(color_);
 	//移動
-	worldTransform_.translation_ += {0.0f, 0.1f, 0.0f};
+	worldTransform_.translation_ += velocity_;//{0.0f, 0.1f, 0.0f}
 	// 行列を定数バッファに転送
 	//worldTransform_.TransferMatrix();
 	//行列を更新
