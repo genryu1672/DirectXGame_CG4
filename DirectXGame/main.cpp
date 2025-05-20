7b55ef3319910216368e2a6c31e637595230dd90
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-05-20 14:10:47 +0900

    CG5_00_02までやった。

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -41,7 +41,7 @@ ID3DBlob* CompileShader(const std::wstring& filePath, const std::string& shaderM
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
 	////エンジンの初期化
-	KamataEngine::Initialize(L"LE3D_08_シマノ_ユウト_CG4");
+	KamataEngine::Initialize(L"LE3D_08_シマノ_ユウト_CG5");
 	
 	//DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
