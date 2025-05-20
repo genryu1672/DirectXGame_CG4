2bb4d3155af08f6274fddaa6f9c507e1d5561dc0
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-05-20 17:11:28 +0900

    CG5_00_02までやった。

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -100,48 +100,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	//塗りつぶしモードをソリッドにする(ワイヤーフレームならD3D12_FILE_MODE_WIREFRAME)
 	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
 
-	////コンパイル済みのShader、エラー時情報の格納場所の用意
-	//ID3DBlob* vsBlob = nullptr;//頂点シェーダーオブジェクト
-	//ID3DBlob* psBlob = nullptr;//ピクセルシェーダオブジェクト
-	//ID3DBlob* errorBlob = nullptr;//エラーオブジェクト
-
-	////頂点シェーダの読み込みとコンパイル
-	//std::wstring vsFile = L"Resources/shaders/TestVS.hlsl";
-	//hr = D3DCompileFromFile(
-	//    vsFile.c_str(), // シェーダファイル名
-	//    nullptr,
-	//    D3D_COMPILE_STANDARD_FILE_INCLUDE,               // インクルード可能にする
-	//    "main", "vs_5_0",                                // エントリーポイント名、シェーダモデル指定
-	//    D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバック用設定
-	//    0, &vsBlob, &errorBlob);
-	//if (FAILED(hr))
-	//{
-	//	DebugText::GetInstance()->ConsolePrintf(std::system_category().message(hr).c_str());
-	//	if (errorBlob)
-	//	{
-	//		DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
-	//	}
-	//	assert(false);
-	//}
-	//
-	////ピクセルシェーダの読み込みとコンパイル
-	//std::wstring psFile = L"Resources/shaders/TestPS.hlsl";
-	//hr = D3DCompileFromFile(
-	//    psFile.c_str(), // シェーダファイル名
-	//    nullptr,
-	//    D3D_COMPILE_STANDARD_FILE_INCLUDE,               // インクルード可能にする
-	//    "main", "ps_5_0",                                // エントリーポイント名、シェーダモデル指定
-	//    D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバック用設定
-	//    0, &psBlob, &errorBlob);
-	//if (FAILED(hr))
-	//{
-	//	DebugText::GetInstance()->ConsolePrintf(std::system_category().message(hr).c_str());
-	//	if (errorBlob)
-	//	{
-	//		DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
-	//	}
-	//	assert(false);
-	//}
 
 	// 頂点シェーダの読み込みとコンパイル
 	ID3DBlob* vsBlob = CompileShader(L"Resources/shaders/TestVS.hlsl", "vs_5_0");
@@ -250,10 +208,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	vertexResource->Release();
 	graphicsPiplineState->Release();
 	signatureBlob->Release();
-	/*if (errorBlob)
-	{
-		errorBlob->Release();
-	}*/
+	
 	rootSignature->Release();
 	vsBlob->Release();
 	psBlob->Release();
