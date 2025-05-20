b82504eb9b27036f884fe4180634a3ba39721a86
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-05-20 14:22:46 +0900

    CG5_00_01までやった。

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -10,7 +10,7 @@ using namespace KamataEngine;
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
 	////エンジンの初期化
-	KamataEngine::Initialize(L"LE3D_08_シマノ_ユウト_CG4");
+	KamataEngine::Initialize(L"LE3D_08_シマノ_ユウト_CG5");
 	
 	//DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
