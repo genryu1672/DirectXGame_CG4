88ae20904ce85e7c033ec26e03bbc57a62d2615d
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-05-21 12:24:49 +0900

    CG5_00_04 P10までやった。

--- DirectXGame/MiscUtility.cpp
+++ DirectXGame/MiscUtility.cpp
@@ -1,8 +1,1 @@
-#pragma once
-#include<string>//wstring,string
 
-//string=>wstring　文字列変換
-std::wstring ConvertString(const std::string& str);
-
-// wstring=>string　文字列変換
-std::string ConvertString(const std::wstring& str);
