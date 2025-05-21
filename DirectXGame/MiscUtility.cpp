86693f0b46c41b3ff073e762b4e8290924a0aa94
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-05-21 12:54:25 +0900

    CG5_00_04 P11までやった。

--- DirectXGame/MiscUtility.cpp
+++ DirectXGame/MiscUtility.cpp
@@ -1,1 +1,20 @@
+#include"MiscUtility.h"
+#include<windows.h>//MuliByteToWideChar WideCharToMultiByte CP_UTF8
+#include<string>//string wstring
 
+//string=>wstring 文字列変換
+std::wstring ConvertString(const std::string& str)
+{
+	if (str.empty())
+	{
+		return std::wstring();
+	}
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0)
+	{
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size(), &result[0]), sizeNeeded);
+	return result;
+}
