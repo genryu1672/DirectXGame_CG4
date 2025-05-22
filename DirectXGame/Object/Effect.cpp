7a5339178af3e00c23134216eb2634b119182c13
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-05-22 12:57:41 +0900

    CG4_03_05までやった。

--- DirectXGame/Object/Effect.cpp
+++ DirectXGame/Object/Effect.cpp
@@ -1,12 +1,14 @@
 #include "Effect.h"
 using namespace KamataEngine;
 #include <random>
+using namespace MathUtility;
 
-void Effect::Initialize(KamataEngine::Model* model, KamataEngine::Vector3 pos) {
+void Effect::Initialize(KamataEngine::Model* model, KamataEngine::Vector3 pos, KamataEngine::Vector4 color) {
 	std::random_device seed_Generator;
 	std::mt19937 RandomEngine(seed_Generator());
 	std::uniform_real_distribution<float> RandomSize(0.0f, 1.0f);
 	std::uniform_real_distribution<float> RandomRotation(-1.0f, 1.0f);
+	std::uniform_real_distribution<float> RandomColor(0.0f, 255.0f);
 	// NULLポインタチェック
 	assert(model);
 	model_ = model;
@@ -18,19 +20,20 @@ void Effect::Initialize(KamataEngine::Model* model, KamataEngine::Vector3 pos) {
 	worldTransform_.translation_ = pos;
 	// 色の初期化
 	objectColor_.Initialize();
-	color_ = Vector4(1, 1, 1, 1);
+	color_ = color;
 	// ワールド変換データの初期化
 	worldTransform_.Initialize();
 }
 
 void Effect::Update() {
+	worldTransform_.rotation_.y += moveSpeed_;
+	worldTransform_.translation_.x += moveSpeed_;
+	worldTransform_.translation_.y += moveSpeed_;
+
 	// 終了なら何もしない
 	if (isFinished_) {
 		return;
 	}
-
-	// カウンターを1フレーム粉の秒数進める
-	counter_ += 1.0f / 60.0f;
 
 	// 存在時間の上限に達したら
 	if (counter_ >= kDuration) {
@@ -40,8 +43,6 @@ void Effect::Update() {
 	}
 	// 色変更オブジェクトに色の数値を設定する
 	objectColor_.SetColor(color_);
-	// フェード処理
-	color_.w = std::clamp(1.0f - counter_ / kDuration, 0.0f, 1.0f);
 
 	worldTransform_.UpdateMatrix();
 }
