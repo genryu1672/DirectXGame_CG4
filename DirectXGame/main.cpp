fe761f330cce83a5823867d00deb661b4c3fb354
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-06-25 12:50:41 +0900

    CG5_00_09 p21までやった。

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -115,13 +115,40 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		device,WinApp::kWindowWidth,WinApp::kWindowHeight,
 		DXGI_FORMAT_R8G8B8A8_UNORM_SRGB,kRenderTargetClearColor
 	);
+
+	//0.DepthStencilTextureResourceの作成-----------------
+	ID3D12Resource* depthStencilResource = CreateDepthStencilTextureResource(
+		device,WinApp::kWindowWidth,WinApp::kWindowHeight
+	);
+
+	//1.DSV用のDescriptorHeapの作成
+	ID3D12DescriptorHeap* dsvDescriptorHeap = nullptr;
+	D3D12_DESCRIPTOR_HEAP_DESC dsvDescriptorHeapDesc{};
+	dsvDescriptorHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;//Heap Type
+	dsvDescriptorHeapDesc.NumDescriptors = 1;//Heap Typeの個数
+	dsvDescriptorHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;//DSVはShaderで触らないとする
+	HRESULT hr;
+	hr = device->CreateDescriptorHeap(&dsvDescriptorHeapDesc, IID_PPV_ARGS(&dsvDescriptorHeap));
+	assert(SUCCEEDED(hr));
+
+	//CPU側からみたHANDLEを取得しておく
+	D3D12_CPU_DESCRIPTOR_HANDLE dsvHandleCPU = dsvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+
+	//2.DSV用のViewの生成
+	D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc{};
+	dsvDesc.Format = DXGI_FORMAT_D32_FLOAT;//基本的にResourceに合わせる
+	dsvDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;//2DTexture
+
+	//DSVHeapの先頭にDSVを作る
+	device->CreateDepthStencilView(depthStencilResource, &dsvDesc, dsvHandleCPU);
+
+
 	//1.RTV用のDescriptorHeapを作成する-----------------------------------
 	ID3D12DescriptorHeap* rtvDescriptorHeap = nullptr;
 	D3D12_DESCRIPTOR_HEAP_DESC rtvDescriptorHeapDesc{};
 	rtvDescriptorHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;//RTV
 	rtvDescriptorHeapDesc.NumDescriptors = 1;//Descriptorの個数は１
 	
-	HRESULT hr;//資料に載ってないから追加した（１０ページ）
 	hr = device->CreateDescriptorHeap(&rtvDescriptorHeapDesc, IID_PPV_ARGS(&rtvDescriptorHeap));
 	assert(SUCCEEDED(hr));
 
