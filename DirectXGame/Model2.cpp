ee805fe2a8a142c0d08a15647da7847510df9a0c
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-06-27 12:49:21 +0900

    CG4_04_02までやった。

--- DirectXGame/Model2.cpp
+++ DirectXGame/Model2.cpp
@@ -63,6 +63,37 @@ Model2* Model2::CreateSquare() {
 
 	vertices.resize(kNumVertices);
 	indices.resize(kNumIndices);
+
+	// 左下
+	vertices[0].pos = {-1.0f, -1.0f, 0.0f};
+	vertices[0].uv = {0.0f, 1.0f};
+
+
+	// 左上
+	vertices[1].pos = {-1.0f, 1, 0.0f};
+	vertices[1].uv = {0.0f, 0.0f};
+
+	// 右下
+	vertices[2].pos = {1, -1.0f, 0.0f};
+	vertices[2].uv = {1.0f, 1.0f};
+
+	// 右上
+	vertices[3].pos = {1, 1, 0.0f};
+	vertices[3].uv = {1.0f, 0.0f};
+
+
+	// インデックス
+	indices[0] = 0;
+	indices[1] = 1;
+	indices[2] = 2;
+
+	indices[3] = 2;
+	indices[4] = 1;
+	indices[5] = 3;
+
+	instance->InitializeFromVertices(vertices, indices);
+
+	return instance;
 }
 
 Model2* Model2::CreateSphere(uint32_t divisionVertial, uint32_t divisionHorizontal) {
