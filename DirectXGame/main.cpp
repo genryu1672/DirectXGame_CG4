da6c19b2e25708846358284a58447d049f459fbb
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-07-04 10:46:19 +0900

    CG4_04_02までやった。

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -18,6 +18,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	//ゲームシーンの初期化
 	gameScene->Initialize();
 
+
 	//メインループ
 	while (true) {
 		//エンジンの更新
@@ -25,6 +26,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		{
 			break;
 		}
+
 		// ゲームシーンの更新
 		gameScene->Update();
 		// 描画開始
