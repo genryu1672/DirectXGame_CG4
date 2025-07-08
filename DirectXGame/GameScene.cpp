51cc082649212e55abe38a2dfff7809a7efbc936
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2025-07-08 10:32:41 +0900

    CG4_05_02をやるにあたって01_03の資料を見てやってるがP7を書くとエラーが出た。

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -1,8 +1,30 @@
 #include "GameScene.h"
 using namespace KamataEngine;
 
-void GameScene::Initialize() {}
+GameScene::GameScene() {}
+
+GameScene::~GameScene() {
+	// デストラクタ
+	delete sprite_;
+}
+
+void GameScene::Initialize() {
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("Resources/Title.png");
+}
 
 void GameScene::Update() {}
 
-void GameScene::Draw() {}
+void GameScene::Draw() 
+{
+	//DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	//スプライト描画前処理
+	Sprite::PreDraw(dxCommon->GetCommandList());
+	
+	//スプライトインスタンスの描画処理
+	sprite_->Draw();
+
+	//スプライト描画後処理
+	Sprite::PostDraw();
+}
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,8 +1,16 @@
 #pragma once
 #include"KamataEngine.h"
+#include "KamataEngine/Sprite.h" // Sprite クラスの定義があるヘッダー
 class GameScene 
 {
 public:
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
 	//初期化
 	void Initialize();
 	//更新
@@ -10,4 +18,12 @@ public:
 
 	//描画
 	void Draw();
+private:
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	//スプライト
+	KamataEngine::Sprite* sprite_ = nullptr;
+	//スプライトインスタンスの生成
+	sprite_ = KamataEngine::Sprite::Create(textureHandle_, {1280, 720});
 };
